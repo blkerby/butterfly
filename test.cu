74824fd594f71350ae12895f6246c19a34e8ab04
Brent Kerby <bkerby@snapfinance.com>
2019-10-02 19:55:02 -0600

    continue developing

--- test.cu
+++ test.cu
@@ -1,10 +1,14 @@
+#include <ATen/ATen.h>
+#include <vector>
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+#include <vector>
+
 #include <stdio.h>
 #include <time.h>
 #include <math.h>
-
-#define THREADS_PER_BLOCK 512
-#define BLOCKS_PER_GRID 5000
-#define WORDS_PER_THREAD 32
 
 
 #define gpuCheck(f) { gpuCheckFunc((f), __FILE__, __LINE__); }
@@ -16,7 +20,8 @@ inline void gpuCheckFunc(cudaError_t err, const char *file, int line){
    }
 }
 
-__device__ float reduce_add_block(float x, float *s_tmp) {
+template <typename scalar_t>
+__device__ scalar_t reduce_add_block(scalar_t x, scalar_t *s_tmp) {
     // First reduce across the warp:
     for (int m = 1; m < 32; m <<= 1){
         x += __shfl_xor_sync(0xffffffff, x, m);
@@ -38,65 +43,122 @@ __device__ float reduce_add_block(float x, float *s_tmp) {
     return x;
 }
 
-__device__ void reduce_add_global(float x, float *s_tmp, float *g_out) {
+template <typename scalar_t>
+__device__ void reduce_add_global(scalar_t x, scalar_t *s_tmp, scalar_t *g_out) {
     x = reduce_add_block(x, s_tmp);
     atomicAdd(g_out, x);
 }
 
 
-// Saturates the GPU memory bandwidth (so throughput of ~280-290 GB/s on Titan V),
-// with very low utilization of the ALUs. To get better efficiency we would need to fuse
-// multiple layers together to reduce the amount of GPU memory loads and stores.
-__global__ void butterfly_forward_slow(
-    const float *data_in,
-    const float *angles,
-    float *data_out,
-    int data_stride
+// Saturates the GPU global memory bandwidth with very low utilization of the ALUs. 
+// To get better efficiency we would need to fuse multiple layers together to 
+// reduce the amount of GPU memory loads and stores.
+template <typename scalar_t>
+__global__ void cuda_butterfly_forward_slow_kernel(
+    const scalar_t *data_in,
+    const scalar_t *angles,
+    scalar_t *data_out,
+    int data_stride,
+    int half_width
 ) {
     // Load the angle for this thread's switch, and compute the corresponding weights.
-    float angle = angles[blockIdx.y];
-    float a = cos(angle);
-    float b = sin(angle);
+    scalar_t angle = angles[blockIdx.y];
+    scalar_t a = cos(angle);
+    scalar_t b = sin(angle);
     
-    // Load the input data
-    int data_idx = 2 * blockIdx.y * data_stride + threadIdx.x + blockDim.x * blockIdx.x;
-    float x = data_in[data_idx];
-    float y = data_in[data_idx + data_stride];
+    // Load the input data from GPU global memory
+    int data_idx_in = 2 * blockIdx.y * data_stride + threadIdx.x + blockDim.x * blockIdx.x;
+    scalar_t x0 = data_in[data_idx_in];
+    scalar_t y0 = data_in[data_idx_in + data_stride];
 
-    // Write the output data
-    data_out[data_idx] = a * x + b * y;
-    data_out[data_idx + data_stride] = -b * x + a * y;
+    // Compute the output data
+    scalar_t x1 = a * x0 + b * y0;
+    scalar_t y1 = -b * x0 + a * y0;
+
+    // Write the output data to GPU global memory
+    int data_idx_out = blockIdx.y * data_stride + threadIdx.x + blockDim.x * blockIdx.x;
+    data_out[data_idx_out] = x1;
+    data_out[data_idx_out + data_stride * half_width] = y1;
 }
 
-__global__ void butterfly_backward_slow(
-    const float *data_in,
-    const float *angles,
-    const float *grad_in,
-    float *grad_out,
-    float *angles_grad_accum,
-    int data_stride
+template <typename scalar_t>
+__global__ void cuda_butterfly_backward_slow_kernel(
+    const scalar_t *data_in,
+    const scalar_t *angles,
+    const scalar_t *grad_in,
+    scalar_t *grad_out,
+    scalar_t *grad_angles_accum,
+    int data_stride,
+    int half_width
 ) {
     // Load the angle for this thread's switch, and compute the corresponding weights.
-    float angle = angles[blockIdx.y];
-    float a = cos(angle);
-    float b = sin(angle);
+    scalar_t angle = angles[blockIdx.y];
+    scalar_t a = cos(angle);
+    scalar_t b = sin(angle);
     
     // Load the input gradient
-    int data_idx = 2 * blockIdx.y * data_stride + threadIdx.x + blockDim.x * blockIdx.x;
-    float dx = grad_in[data_idx];
-    float dy = grad_in[data_idx + data_stride];
+    int data_idx_in = blockIdx.y * data_stride + threadIdx.x + blockDim.x * blockIdx.x;
+    scalar_t dx1 = grad_in[data_idx_in];
+    scalar_t dy1 = grad_in[data_idx_in + data_stride * half_width];
 
-    // Write the output gradient for continuing backpropagation into earlier layers
-    grad_out[data_idx] = a * dx - b * dy;
-    grad_out[data_idx + data_stride] = b * dx + a * dy;
+    // Compute the output gradient for continuing backpropagation into earlier layers
+    scalar_t dx0 = a * dx1 - b * dy1;
+    scalar_t dy0 = b * dx1 + a * dy1;
+
+    // Write the output gradient to GPU global memory
+    int data_idx_out = 2 * blockIdx.y * data_stride + threadIdx.x + blockDim.x * blockIdx.x;
+    grad_out[data_idx_out] = dx0;
+    grad_out[data_idx_out + data_stride] = dy0;
 
     // Accumulate the gradient for the angles in the current layer
-    __shared__ float tmp[32];
-    float x = data_in[data_idx];
-    float y = data_in[data_idx + data_stride];
-    float g = y*dx - x*dy;
-    reduce_add_global(g, tmp, &angles_grad_accum[blockIdx.y]);
+    __shared__ scalar_t tmp[32];
+    scalar_t x1 = data_in[data_idx_in];
+    scalar_t y1 = data_in[data_idx_in + data_stride * half_width];
+    scalar_t g = y1*dx1 - x1*dy1;
+    reduce_add_global(g, tmp, &grad_angles_accum[blockIdx.y]);
 }
+
+void cuda_butterfly_forward_slow(at::Tensor data_in, at::Tensor angles, at::Tensor data_out) {
+    int dimBlock = 256;
+    dim3 dimGrid(data_in.size(1) / dimBlock, angles.size(0));
+
+    AT_DISPATCH_FLOATING_TYPES(data_in.type(), "test_cuda_double", ([&] {
+        cuda_butterfly_forward_slow_kernel<scalar_t><<<dimGrid, dimBlock>>>(
+            data_in.data<scalar_t>(),
+            angles.data<scalar_t>(),
+            data_out.data<scalar_t>(),
+            data_in.size(1),
+            data_in.size(0) / 2
+        );
+        gpuCheck( cudaGetLastError() )
+    }));
+}
+
+
+void cuda_butterfly_backward_slow(
+    at::Tensor data_in,
+    at::Tensor angles, 
+    at::Tensor grad_in,
+    at::Tensor grad_out,
+    at::Tensor grad_angles_accum
+) {
+    int dimBlock = 256;
+    dim3 dimGrid(data_in.size(1) / dimBlock, angles.size(0));
+
+    AT_DISPATCH_FLOATING_TYPES(data_in.type(), "test_cuda_double", ([&] {
+        cuda_butterfly_backward_slow_kernel<scalar_t><<<dimGrid, dimBlock>>>(
+            data_in.data<scalar_t>(),
+            angles.data<scalar_t>(),
+            grad_in.data<scalar_t>(),
+            grad_out.data<scalar_t>(),
+            grad_angles_accum.data<scalar_t>(),
+            data_in.size(1),
+            data_in.size(0) / 2
+        );
+        gpuCheck( cudaGetLastError() )
+    }));
+}
+
 
 // int main() {
 //     float *h_angles;
